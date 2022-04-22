81a161b2f0276fd3cafc26eafb0351cd8c1c9908
bertalanV <bertalan.verebelyi@gmail.com>
2022-04-23 01:21:12 +0200

    battle system update texts

--- Assets/Scripts/Pokemons/Move.cs
+++ Assets/Scripts/Pokemons/Move.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Move
 {
     public MoveBase Base { get; set; }
-    public int Pp { get; set; }
+    public int PP { get; set; }
 
     public Move(MoveBase pBase)
     {
         Base = pBase;
-        Pp = pBase.PP;
+        PP = pBase.PP;
     }
 }
