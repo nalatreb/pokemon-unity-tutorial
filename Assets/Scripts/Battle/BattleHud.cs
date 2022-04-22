8d6f12b203cd3484e49eef0fb71f997fe16c4640
bertalanV <bertalan.verebelyi@gmail.com>
2022-04-23 00:30:32 +0200

    add battle scene

---
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BattleHud : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] HPBar hpBar;
+
+    public void SetData(Pokemon pokemon)
+    {
+        nameText.text = pokemon.Base.Name;
+        levelText.text = "Lvl " + pokemon.Level;
+        hpBar.SetHP((float) pokemon.HP / pokemon.MaxHp);
+    }
+}
